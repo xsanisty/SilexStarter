be19ed73394b8b7b99a195ff10ec46853bf1a776
Ikhsan Agustian <ikhsan017@gmail.com>
2015-02-10 09:46:22 +0700

    update config container, introduce module::config getter

--- src/SilexStarter/Config/ConfigurationContainer.php
+++ src/SilexStarter/Config/ConfigurationContainer.php
@@ -9,37 +9,53 @@ class ConfigurationContainer implements ArrayAccess{
 
     protected $config;
     protected $app;
-    protected $basepath;
+    protected $configPath = [];
+    protected $namespacedPath = [];
 
     /**
      * ConfigurationContainer constructor
      * @param Application $app      [instance of Silex Application]
-     * @param string      $basepath [the path where configuration file located]
+     * @param string      $configPath [the path where configuration file located]
      */
-    public function __construct(Application $app, $basepath){
-        $this->basepath = rtrim($basepath, DIRECTORY_SEPARATOR);
-        $this->config   = [];
-        $this->app      = $app;
+    public function __construct(Application $app, $configPath){
+        $this->configPath[] = rtrim($configPath, DIRECTORY_SEPARATOR);
+        $this->config       = [];
+        $this->app          = $app;
     }
 
     /**
      * Load the configuration file and save the value into array container
-     * @param  [string] $file       [description]
+     * @param  [string] $file       filename or namespace::filename
      * @param  [string] $configKey  [description]
      * @return [void]               [description]
      */
     public function load($file, $configKey = null){
-        $file       = ('.php' == substr($file, -4, 4)) ? $file : $file.'.php';
+        $fileChunk  = explode('::', $file, 2);
+        $namespace  = (count($fileChunk) > 1) ? $fileChunk[0] : null;
+        $filename   = (count($fileChunk) > 1) ? $fileChunk[1] : $fileChunk[0];
+        $filename   = ('.php' == substr($filename, -4, 4)) ? $filename : $filename.'.php';
         $configKey  = (!$configKey) ? explode('.', $file)[0] : $configKey;
-        $filePath   = $this->basepath.'/'.$file;
+        $filePath   = null;
 
         /** return immediately when config already loaded */
         if(isset($this->config[$configKey])){
             return;
         }
 
-        if(!file_exists($filePath)){
-            throw new \Exception("Configuration file [$filePath] can not be found", 1);
+        /** try to load the configuration file */
+        if(is_null($namespace)){
+            foreach($this->configPath as $configPath){
+                if(file_exists($configPath.'/'.$filename)){
+                    $filePath = $configPath.'/'.$filename;
+                    break;
+                }
+            }
+        }elseif(file_exists($this->namespacedPath[$namespace].'/'.$filename)){
+            $filePath = $this->namespacedPath[$namespace].'/'.$filename;
+        }
+
+        if(is_null($filePath)){
+            throw new \Exception("Configuration file [$file] can not be found", 1);
         }
 
         if($configKey == 'app'){
@@ -57,17 +73,29 @@ class ConfigurationContainer implements ArrayAccess{
         }
     }
 
+    /**
+     * remove configuration key from the configuration container
+     * @param  [type] $offset [description]
+     * @return [type]         [description]
+     */
     public function unload($offset){
         $this->offsetUnset($offset);
     }
 
     /**
-     * [loadDirectory description]
-     * @param  [type] $dir [description]
-     * @return [type]      [description]
+     * add config directory into config  directory lists
+     * @param  string $dir          the config directory
+     * @param  string $namespace    config namespace
+     * @return void
      */
-    public function loadDirectory($dir = null){
+    public function addDirectory($directory, $namespace = null){
+        $directory = rtrim($directory, '/');
 
+        if(is_null($namespace)){
+            $this->configPath[] = $directory;
+        }else{
+            $this->namespacedPath[$namespace] = $directory;
+        }
     }
 
     public function set($offset, $value){
@@ -93,8 +121,9 @@ class ConfigurationContainer implements ArrayAccess{
             return $this->config[$offset];
         }
 
-        /** support module:file.key.subkey */
-        $offsetChunk[0] = str_replace(':', '/', $offsetChunk[0]);
+        /** support module:file.key.subkey
+        $offsetChunk[0] = str_replace('::', '/', $offsetChunk[0]);
+        */
 
         /** if not set, try to load the config file */
         if(!isset($this->config[$offsetChunk[0]])){

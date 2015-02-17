a573ebd73862ec4735ff3cae8d7b0f79293356eb
Ikhsan Agustian <ikhsan017@gmail.com>
2015-02-17 10:06:14 +0700

    update index

--- public/index.php
+++ public/index.php
@@ -1,13 +1,4 @@
 <?php
 
-function convert($size)
-{
-    $unit=array('b','kb','mb','gb','tb','pb');
-    return @round($size/pow(1024,($i=floor(log($size,1024)))),2).' '.$unit[$i];
-}
-
-//echo convert(memory_get_usage(true)).'<br />';
 $app = require '../app/bootstrap/start.php';
-//echo convert(memory_get_usage(true)).'<br />';
 $app->run();
-//echo convert(memory_get_usage(true)).'<br />';

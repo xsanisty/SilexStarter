5c6c3773b8cb9a7eb7da7cf5ac6efe67e28294d8
Ikhsan Agustian <ikhsan017@gmail.com>
2015-02-17 09:11:15 +0700

    add some docs to the route file

--- app/routes.php
+++ app/routes.php
@@ -7,15 +7,24 @@
  * Route::get('/', 'SomeNamespace\OtherController:index');
  *
  * Sample group route
- *  Route::group('group', function($route){
- *      $route->get('/', 'GroupController:index');
- *      $route->get('/list', 'GroupController:lists');
- *      $route->get('/test', 'GroupController:test');
- *  });
+ * Route::group('group', function($route){
+ *     $route->get('/', 'GroupController:index');       //accessible via domain.tld/group/ (with the trailing slash)
+ *     $route->get('/list', 'GroupController:lists');   //accessible via domain.tld/group/list
+ *     $route->get('/test', 'GroupController:test');    //accessible via domain.tld/group/test
+ * });
  *
  * Sample resource route
- * Route::resource('books', 'BookController');
+ * Route::resource('book', 'BookController');           //Similar to laravel's Route::resource, this will map
+ *                                                      //GET    /book/         => BookController::index
+ *                                                      //GET    /book/id       => BookController::show($id)
+ *                                                      //GET    /book/id/edit  => BookController::edit($id)
+ *                                                      //GET    /book/create   => BookController::create
+ *                                                      //POST   /book/         => BookController::store
+ *                                                      //PUT    /book/id       => BookController::update($id)
+ *                                                      //DELETE /book/id       => BookController::destroy($id)
  *
  * Sample controller route
- * Route::controller('auto', 'AutoRouteController');
+ * Route::controller('auto', 'AutoRouteController');    //method getIndex() will be accessible via /auto/ (with the trailing slash)
+ *                                                      //method getSomePath will be accessible via /auto/some_path
+ *                                                      //method deleteResource will be accessible via DELETE /auto/resource
  */

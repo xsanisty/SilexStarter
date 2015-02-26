304ce62f229f3d04fd07e6170207553e97763851
Ikhsan Agustian <ikhsan017@gmail.com>
2015-02-26 19:44:21 +0700

    add filesystem static proxy and service provider

---
+++ src/SilexStarter/Provider/FilesystemServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace SilexStarter\Provider;
+
+use Silex\Application;
+use Silex\ServiceProviderInterface;
+use Symfony\Component\Filesystem\Filesystem;
+
+class FilesystemServiceProvider implements ServiceProviderInterface{
+
+    public function register(Application $app){
+        $app['filesystem'] = $app->share(function(){
+            return new Filesystem;
+        });
+    }
+
+    public function boot(Application $app){
+
+    }
+}
